86debe62d427860bb9aec58a01f7999f03345bf6
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2023-11-05 00:45:38 -0500

    a

--- assimp_viewer/main.cpp
+++ assimp_viewer/main.cpp
@@ -1,5 +1,5 @@
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 
@@ -11,24 +11,24 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <filesystem>
 
-#include <model.h>
-#include <shader_m.h>
 #include <animation.h>
-#include <grid.h>
-#include <dev_gui.h>
-#include <skybox.h>
 #include <collision.h>
+#include <dev_gui.h>
+#include <grid.h>
+#include <model.h>
 #include <my_math.h>
+#include <shader_m.h>
+#include <skybox.h>
 #include <terrain.h>
 
-#include <scene_graph.h>
 #include <log_file_functions.h>
+#include <scene_graph.h>
 
 // settings
 unsigned int SCR_WIDTH = 2000;
@@ -52,7 +52,6 @@ void processInput(GLFWwindow* window, Camera* camera);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
-
 float acceleration = 0.15f;
 float max_speed = 10.0f;
 float friction = 2.0f;
@@ -62,30 +61,51 @@ bool jumpDebounce = false;
 bool isJumping = false;
 float jumpDuration = 0.7f;
 
+typedef struct PlayerState {
+    glm::vec3 position;
+    glm::vec3 velocity;
+    float force;
+    float mass;
+} PlayerState;
+
+void integrate(PlayerState& state, float& time, float dt) {
+
+    glm::vec3 velocity = state.velocity;
+    glm::vec3 position = state.position;
+    float force = state.force;
+    float mass = state.mass;
+
+    // semi-implicit euler
+    while (time <= 10.0) {
+        velocity = velocity + (force / mass) * dt;
+        position = position + velocity * dt;
+        time += dt;
+    }
+}
+
 int main()
 {
     GLFWwindow* window = InitializeWindow();
     PlayerCamera = CreateCameraVector(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), YAW, PITCH);
-    
+
     unsigned int grid_VAO = LoadGrid();
 
-    unsigned int basicShader  = createShader(filepath("/shaders/basic/basic.vs"), filepath("/shaders/basic/basic.fs"));
+    unsigned int basicShader = createShader(filepath("/shaders/basic/basic.vs"), filepath("/shaders/basic/basic.fs"));
     shaderIdArray[2] = basicShader;
     unsigned int modelShader = createShader(filepath("/shaders/6.multiple_lights.vs"), filepath("/shaders/6.multiple_lights.fs"));
     shaderIdArray[0] = modelShader;
-    unsigned int animShader   = createShader(filepath("/shaders/anim_model.vs"),  filepath("/shaders/anim_model.fs"));
-    unsigned int gridShader   = createShader(filepath("/shaders/grid.vs"),        filepath("/shaders/grid.fs"));
+    unsigned int animShader = createShader(filepath("/shaders/anim_model.vs"), filepath("/shaders/anim_model.fs"));
+    unsigned int gridShader = createShader(filepath("/shaders/grid.vs"), filepath("/shaders/grid.fs"));
     unsigned int hitboxShader = createShader(filepath("/shaders/hitbox.vs"), filepath("/shaders/hitbox.fs"));
     shaderIdArray[1] = hitboxShader;
     unsigned int animatedShader = createShader(filepath("/shaders/animated_texture.vs"), filepath("/shaders/animated_texture.fs"));
-    //unsigned int lightShader  = createShader(filepath("/shaders/6.multiple_lights.vs"), filepath("/shaders/6.multiple_lights.fs"));
+    // unsigned int lightShader  = createShader(filepath("/shaders/6.multiple_lights.vs"), filepath("/shaders/6.multiple_lights.fs"));
 
     unsigned int billboardShader = createShader(filepath("/shaders/billboard.vs"), filepath("/shaders/billboard.fs"));
 
     Model* billboard = LoadModel(filepath("/resources/models/billboards/hp1.obj"));
-    Model* moon = LoadModel(filepath("/resources/models/billboards/moon.obj")); 
+    Model* moon = LoadModel(filepath("/resources/models/billboards/moon.obj"));
     Model* sun = LoadModel(filepath("/resources/models/billboards/sun.obj"));
-
 
     Model* player = LoadModel(filepath("/resources/objects/vampire/dancing_vampire.dae"));
 
@@ -95,17 +115,17 @@ int main()
 
     Model* arrow = LoadModel(filepath("/resources/models/direction_arrows/z.obj"));
 
-    Model* vampire = LoadModel(filepath("/resources/models/zelda/character/soldier.dae"));
+    Model* vampire = LoadModel(filepath("/resources/objects/vampire/dancing_vampire.dae"));
 
-    //Model* cube = LoadModel(filepath("/resources/models/cube/cube_outline.obj"));
-    //unsigned int cube = CreateHitbox();
+    // Model* cube = LoadModel(filepath("/resources/models/cube/cube_outline.obj"));
+    // unsigned int cube = CreateHitbox();
 
     if (LoadScene(filepath("/resources/scenes/scene1.json"))) {
         printf("LoadScene Failed!\n");
     }
 
     LoadSkybox(filepath, "skybox7");
-    //LoadTerrain(filepath, filepath("/resources/textures/heightmaps/map1.png"));
+    // LoadTerrain(filepath, filepath("/resources/textures/heightmaps/map1.png"));
 
     glm::vec3 color = glm::vec3(0.0f);
 
@@ -113,30 +133,106 @@ int main()
     double lastSpacePressTime = 0.0;
 
     bool isSpaceKeyPressed = false;
-    float maxJumpDuration = 2.5f;  // Adjust this to control the jump duration (in seconds)
+    float maxJumpDuration = 2.5f; // Adjust this to control the jump duration (in seconds)
     float jumpStartTime = 0.0f;
 
-     glm::vec3 pointLightPositions[] = {
+    glm::vec3 pointLightPositions[] = {
         glm::vec3(0.7f, 0.2f, 2.0f),
         glm::vec3(2.3f, -3.3f, -4.0f),
         glm::vec3(-4.0f, 2.0f, -12.0f),
         glm::vec3(0.0f, 0.0f, -3.0f)
     };
 
-   
+    // Physics
+
+
+    float t = 0.0;
+    float dt = 0.01;
+
+    float currentTime = hires_time_in_seconds();
+    float accumulator = 0.0;
+
+    PlayerState previousState;
+    PlayerState currentState;
+
+
     while (!glfwWindowShouldClose(window)) {
 
         // per-frame time logic
         // --------------------
-        float currentTime = glfwGetTime();
+        //float currentTime = glfwGetTime();
         deltaTime = currentTime - previousTime;
         previousTime = currentTime;
+
+        float newTime = glfwGetTime();
+        float frameTime = newTime - currentTime;
+        if (frameTime > 0.25)
+            frameTime = 0.25;
+        float currentTime = newTime;
+
+        accumulator += frameTime;
+
+        while (accumulator >= dt)
+        {
+            previousState = currentState;
+            integrate(currentState, t, dt);
+            t += dt;
+            accumulator -= dt;
+        }
+
+        const double alpha = accumulator / dt;
+
+        PlayerState state = currentState * alpha + previousState * (1.0 - alpha);
+
+        glm::vec3 vector;
+
+        if (isMoving) {
+            playerVelocity += acceleration;
+
+            if (playerVelocity > max_speed)
+                playerVelocity = max_speed;
+
+        } else {
+            playerVelocity -= friction;
+            if (playerVelocity < 0)
+                playerVelocity = 0.0f;
+        }
+
+        if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
+            if (currentTime - lastSpacePressTime >= debounceDelay) {
+                movePlayer(glm::vec3(0.0f, -1.0f, 0.0f));
+                lastSpacePressTime = currentTime;
+            }
+        }
+
+        // need 2 fix dis           ?switch to adding new velocity to old? idk
+        vector = (glm::normalize(directionVector) * playerVelocity) * deltaTime;
+
+        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+            if (currentTime - jumpStartTime >= debounceDelay) {
+                jumpStartTime = currentTime;
+                isJumping = true;
+                vector += glm::vec3(0.0f, 0.5f, 0.0f); // Apply initial y-velocity
+            }
+        }
+        if (isJumping) {
+            // Keep applying y-velocity within the jump duration
+            vector += glm::vec3(0.0f, 0.5f - (currentTime - jumpStartTime), 0.0f);
+
+            if (currentTime - jumpStartTime >= jumpDuration) {
+                isJumping = false; // Stop jumping after jumpDuration
+                // Reset y-velocity when the jump duration is over
+            }
+        }
+
+        // Send to collision detection and response
+        movePlayer(vector);
 
         // input
         // -----
         processInput(window, PlayerCamera);
-      
-        //imgui
+
+        // imgui
         Main_GUI_Loop(currentTime);
 
         // render
@@ -148,7 +244,6 @@ int main()
         glm::mat4 projection = glm::perspective(glm::radians(PlayerCamera->Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, RENDER_DISTANCE);
         glm::mat4 view = GetViewMatrix(*PlayerCamera);
 
-        
         /*
         // Draw Grid, with instance array
         glUseProgram(gridShader);
@@ -162,37 +257,34 @@ int main()
         glUseProgram(basicShader);
         setShaderMat4(basicShader, "projection", projection);
         setShaderMat4(basicShader, "view", view);
-       
+
         glUseProgram(animShader);
-        
+
         // (shader, uniformName, value)
         setShaderMat4(animShader, "projection", projection);
         setShaderMat4(animShader, "view", view);
-        
+
         if (animationPlaying) {
             AnimateModel(deltaTime, player->m_Animations[0], player->rootSkeletonNode, player->m_FinalBoneMatrices);
         }
-        
+
         for (int i = 0; i < 100; ++i)
             setShaderMat4(animShader, "finalBonesMatrices[" + std::to_string(i) + "]", player->m_FinalBoneMatrices[i]);
 
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); 
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
         model = glm::scale(model, glm::vec3(.5f, .5f, .5f));
         setShaderMat4(animShader, "model", model);
 
         DrawModel(vampire, animShader);
-        
 
         glUseProgram(modelShader);
 
         setVec3(modelShader, "viewPos", PlayerCamera->Position);
         setShaderFloat(modelShader, "material.shininess", 32.0f);
-        //setInt(modelShader, "material.diffuse", 0);
-        //setInt(modelShader, "material.specular", 1);
-        //setInt(modelShader, "material.emission", 2);
-
-
+        // setInt(modelShader, "material.diffuse", 0);
+        // setInt(modelShader, "material.specular", 1);
+        // setInt(modelShader, "material.emission", 2);
 
         float radius = 1.0f; // You can adjust the radius of the circle
         float angular_speed = dayNightSpeed; // You can adjust the speed of rotatiom
@@ -208,7 +300,7 @@ int main()
 
         glm::vec3 orange = glm::vec3(1.0f, 0.741f, 0.086f);
         glm::vec3 purple = glm::vec3(0.082f, 0.0f, 0.298f);
-        glm::vec3 white  = glm::vec3(0.9f, 1.0f, 0.9f);
+        glm::vec3 white = glm::vec3(0.9f, 1.0f, 0.9f);
 
         sun_t *= 2.5f;
         if (sun_t > 0 && sun_t < 1) {
@@ -217,7 +309,6 @@ int main()
         if (sun_t < 0 && sun_t > -1) {
             color = lerp(orange, purple, glm::abs(sun_t));
         }
-        
 
         setVec3(modelShader, "dirLight.direction", glm::vec3(-0.5f, -1.0f, 0.0f));
         setVec3(modelShader, "dirLight.ambient", sliderColor.x, sliderColor.y, sliderColor.z);
@@ -263,21 +354,11 @@ int main()
         setShaderMat4(modelShader, "projection", projection);
         setShaderMat4(modelShader, "view", view);
 
-        
-
-
-
-
-
-
-
-
         glLineWidth(1.0f);
         glUseProgram(hitboxShader);
         setShaderMat4(hitboxShader, "projection", projection);
         setShaderMat4(hitboxShader, "view", view);
-        //DrawTerrain(view, projection, sunDirection, color, PlayerCamera->Position);
-
+        // DrawTerrain(view, projection, sunDirection, color, PlayerCamera->Position);
 
         if (polygonMode) {
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -287,81 +368,28 @@ int main()
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         }
 
-
-       // AABB_AABB_Collision(*hitboxes[0].rootAABB, *hitboxes[1].rootAABB, hitboxes[0].m_Matrix, hitboxes[1].m_Matrix);
-        
-
+        // AABB_AABB_Collision(*hitboxes[0].rootAABB, *hitboxes[1].rootAABB, hitboxes[0].m_Matrix, hitboxes[1].m_Matrix);
 
         glUseProgram(animatedShader);
         setShaderMat4(animatedShader, "projection", projection);
         setShaderMat4(animatedShader, "view", view);
 
         glUseProgram(modelShader);
-        
+
         // Player
         model = glm::mat4(1.0f);
         model = glm::translate(model, playerPosition);
 
-        glm::vec3 vector;
-        
-        if (isMoving) {
-            playerVelocity += acceleration;
-
-            if (playerVelocity > max_speed)
-                playerVelocity = max_speed;
-
-        } else {
-            playerVelocity -= friction;
-            if (playerVelocity < 0) playerVelocity = 0.0f;
-        }
-
-        
-
-        if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
-            if (currentTime - lastSpacePressTime >= debounceDelay) {
-                movePlayer(glm::vec3(0.0f, -1.0f, 0.0f));
-                lastSpacePressTime = currentTime;
-            }
-        }
-
-
-        //need 2 fix dis           ?switch to adding new velocity to old? idk
-        vector = (glm::normalize(directionVector) * playerVelocity) * deltaTime;
-
-        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-            if (currentTime - jumpStartTime >= debounceDelay) {
-                jumpStartTime = currentTime;
-                isJumping = true;
-                vector += glm::vec3(0.0f, 0.5f, 0.0f); // Apply initial y-velocity
-            }
-        }
-        if (isJumping) {
-            // Keep applying y-velocity within the jump duration
-            vector += glm::vec3(0.0f, 0.5f - (currentTime - jumpStartTime), 0.0f);
-            
-            if (currentTime - jumpStartTime >= jumpDuration) {
-                isJumping = false; // Stop jumping after jumpDuration
-               // Reset y-velocity when the jump duration is over
-            }
-        }
-
-
-        movePlayer(vector);
-
-
         if (PlayerCamera->Type == THIRDPERSON) {
-           model = glm::rotate(model, playerRotation, glm::vec3(0.0f, 1.0f, 0.0f));
+            model = glm::rotate(model, playerRotation, glm::vec3(0.0f, 1.0f, 0.0f));
         }
 
-        //model = glm::scale(model, glm::vec3(.025f, .025f, .025f));
+        // model = glm::scale(model, glm::vec3(.025f, .025f, .025f));
         setShaderMat4(modelShader, "model", model);
-        //DrawModel(soid_man, modelShader);
-
+        // DrawModel(soid_man, modelShader);
 
         glm::vec3 playerCenter = playerPosition; //+glm::vec3(0.0f, 2.6f, 0.0f);
         glm::vec3 sourcePoint = playerCenter;
-
-         
 
         // BACKFACE CULLING |ON|
         glEnable(GL_CULL_FACE);
@@ -370,15 +398,13 @@ int main()
         setShaderMat4(hitboxShader, "projection", projection);
         setShaderMat4(hitboxShader, "view", view);
 
-
-
         // Collision Point Ball
         model = glm::mat4(1.0f);
         model = glm::translate(model, playerCenter + glm::normalize(vectorPosition));
         model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
         setShaderMat4(hitboxShader, "model", model);
         setShaderVec4(hitboxShader, "color", glm::vec4(0.0f, 1.0f, 1.0f, 1.0f));
-        //DrawModel(sphere, hitboxShader);
+        // DrawModel(sphere, hitboxShader);
 
         // Collision Point Ball
         model = glm::mat4(1.0f);
@@ -386,41 +412,32 @@ int main()
         model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
         setShaderMat4(hitboxShader, "model", model);
         setShaderVec4(hitboxShader, "color", glm::vec4(0.0f, 0.0f, 1.0f, 1.0f));
-        //DrawModel(sphere, hitboxShader);
+        // DrawModel(sphere, hitboxShader);
 
         // Player hitbox
         model = glm::mat4(1.0f);
         model = glm::translate(model, playerCenter);
         setShaderMat4(hitboxShader, "model", model);
         setShaderVec4(hitboxShader, "color", glm::vec4(1.0f, 0.0f, 0.0f, 0.3f));
-        DrawModel(sphere, hitboxShader);    
-
-
-
+        DrawModel(sphere, hitboxShader);
 
         // BACKFACE CULLING |OFF|
         glDisable(GL_CULL_FACE);
-        
-        
+
         setShaderVec4(hitboxShader, "color", glm::vec4(1.0f, 0.0f, 0.5f, 0.5f));
         setShaderMat4(hitboxShader, "model", glm::mat4(1.0f));
 
-        
-        
-
         // Needs to be drawn last; covers up everything after it
-        DrawSkybox(*PlayerCamera, view, projection, currentTime);   
-        //update, except for transparent stuff i guess
+        DrawSkybox(*PlayerCamera, view, projection, currentTime);
+        // update, except for transparent stuff i guess
 
-        
-        
         /*
 
         float scale = 10.0f;
         float x_size = 3;
         float y_size = 1;
         float z_size = 3;
-         
+
         for (int x = -x_size; x <= x_size; x++) {
            for (int y = -y_size; y <= y_size; y++) {
                 for (int z = -z_size; z <= z_size; z++) {
@@ -491,7 +508,6 @@ GLFWwindow* InitializeWindow()
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
-
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 
     // glad: load all OpenGL function pointers
@@ -557,15 +573,13 @@ void processInput(GLFWwindow* window, Camera* camera)
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS && glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
         CameraProcessKeyboard(camera, BACKWARD_LEFT, deltaTime);
 
-	if ((glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) && PlayerCamera->Type == THIRDPERSON && mousePressed) {
-		isMoving = true;
-	} else if ((glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) && PlayerCamera->Type == THIRDPERSON && !mousePressed) {
-		isMoving = true;
-    }
-    else {
+    if ((glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) && PlayerCamera->Type == THIRDPERSON && mousePressed) {
+        isMoving = true;
+    } else if ((glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) && PlayerCamera->Type == THIRDPERSON && !mousePressed) {
+        isMoving = true;
+    } else {
         isMoving = false;
     }
-
 }
 
 // glfw: whenever the mouse moves, this callback is called
@@ -576,9 +590,9 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 
     if (rightMouseButtonState == GLFW_PRESS) {
 
-       mousePressed = true;
-        
-       glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        mousePressed = true;
+
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
         if (firstMouse) {
             lastX = xpos;
@@ -593,9 +607,9 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
         lastY = ypos;
 
         CameraProcessMouseMovement(PlayerCamera, xoffset, yoffset);
-        
+
     } else if (rightMouseButtonState == GLFW_RELEASE) {
-    // Enable the cursor when the right mouse button is released
+        // Enable the cursor when the right mouse button is released
         glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
         firstMouse = true;
         mousePressed = false;
@@ -606,6 +620,5 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 // ----------------------------------------------------------------------
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-    //CameraProcessMouseScroll(PlayerCamera, yoffset);
+    // CameraProcessMouseScroll(PlayerCamera, yoffset);
 }
-
